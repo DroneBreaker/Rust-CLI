16a2e1f96d55215a8b164de27dfe77fad2e8d29f
DroneBreaker <brownierz01@gmail.com>
2024-02-07 16:23:18 +0000

    sent code to modules

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-use text_colorizer::*;
-
-#[derive(Debug)]
-#[allow(dead_code)]
-
-struct Arguments {
-    pattern: String,
-    replace: String,
-    input_file: String,
-    output_file: String
-}
+mod find_and_replace;
 
 fn main() {
     // println!("Hello, world!");
-    print_help()
-}
 
-fn print_help() {
-    eprintln!("{} - replace a string with a new string", "Find and Replace".green());
-    eprintln!("Usage: <target string> <replacement string> <INPUT FILE> <OUTPUT FILE>")
+    find_and_replace::run();
 }
